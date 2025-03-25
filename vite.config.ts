94ce27ba6e8bbd9655b3baf2534fc5487f11fc76
Nidhal <55117372+NidhalNouma@users.noreply.github.com>
2025-03-25 13:25:16 -0400

    Changing project folder name

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Personal-Portfolio-V2/",
+  base: "/Portfolio-V2/",
   server: {
     port: 3000,
     hmr: true,
